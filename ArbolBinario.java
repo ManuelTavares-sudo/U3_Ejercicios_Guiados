7d64b320a94545fc50308ec374864d637893205b
ManuelTavares-sudo <1224100719.math@gmail.com>
2025-11-25 20:24:55 -0600

    Update ArbolBinario.java

--- ArbolBinario.java
+++ ArbolBinario.java
@@ -1,3 +1,7 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package mx.edu.utng.tics.arboles_binarios;
 
 /**
